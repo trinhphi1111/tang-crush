5dd091db472894521c0085d6f7cf7ebc920e220c
trinhphi1111 <86926736+trinhphi1111@users.noreply.github.com>
2021-07-05 00:47:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
+    introDesc: `Trái đất vốn lạ thườg
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
